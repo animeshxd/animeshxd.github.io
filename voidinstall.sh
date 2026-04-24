25245a042fd41498ea6769ebf98ab018ac208e57
Animesh Murmu <47624427+animeshxd@users.noreply.github.com>
2026-04-24 18:53:14 +0530

    Add voidinstall.sh for Void Linux installation
    Script to install Void Linux base system and configure fstab.

---
+++ voidinstall.sh
@@ -0,0 +1,13 @@
+REPO=https://mirror.freedif.org/voidlinux/
+ARCH=x86_64
+
+xbps-install -Su xbps
+
+mkdir -p /mnt/var/db/xbps/keys
+cp /var/db/xbps/keys/* /mnt/var/db/xbps/keys/
+
+XBPS_ARCH=$ARCH xbps-install -S -r /mnt -R "$REPO" base-system
+
+## configure time, etcs
+xgenfstab -U /mnt > /mnt/etc/fstab
+

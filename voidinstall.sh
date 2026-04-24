95f968a5a0b58fade85cba21d6c75d0002c079b0
Animesh Murmu <47624427+animeshxd@users.noreply.github.com>
2026-04-24 19:09:49 +0530

    Add TODOs for post-install configuration steps
    Added TODO comments for various post-install configuration tasks.

--- voidinstall.sh
+++ voidinstall.sh
@@ -8,6 +8,20 @@ cp /var/db/xbps/keys/* /mnt/var/db/xbps/keys/
 
 XBPS_ARCH=$ARCH xbps-install -S -r /mnt -R "$REPO" base-system
 
-## configure time, etcs
+# TODO: configure time, language, etcs
 xgenfstab -U /mnt > /mnt/etc/fstab
+
+#TODO:  configure Hardware clock localtime, install ntpd
+
+#TODO: install systemd-boot, vim, openssh, 
+
+#TODO: tigger linux post-install hook for systemd-boot (xbps-reconfigure -f linux6.12)
+
+ln -s /mnt/etc/sv/dhcpcd /mnt/var/service/
+ln -s /mnt/etc/sv/sshd /mnt/var/service/
+
+#TODO: start ntpd
+
+#TODO: change password of root
+
 

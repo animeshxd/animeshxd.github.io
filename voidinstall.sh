e9dda03677811b153009777c0d51faccb5301063
Animesh Murmu <47624427+animeshxd@users.noreply.github.com>
2026-04-27 20:31:21 +0530

    Refactor base system installation in voidinstall.sh
    Define a base system array and use it for installation.

--- voidinstall.sh
+++ voidinstall.sh
@@ -6,7 +6,19 @@ xbps-install -Su xbps
 mkdir -p /mnt/var/db/xbps/keys
 cp /var/db/xbps/keys/* /mnt/var/db/xbps/keys/
 
-XBPS_ARCH=$ARCH xbps-install -S -r /mnt -R "$REPO" base-system
+readonly BASE_SYSTEM=(
+  linux linux-firmware linux-firmware-amd wifi-firmware systemd-boot kmod
+  base-files glibc-locales tzdata iana-etc runit-void eudev acpid kbd libgcc
+  xbps removed-packages
+  bash dash bash-completion nvi vim
+  shadow sudo
+  coreutils findutils grep gzip sed gawk less which file procps-ng util-linux usbutils
+  e2fsprogs dosfstools
+  iproute2 iputils dhcpcd iwd openssh ntpd traceroute
+  man-pages mdocml
+)
+
+XBPS_ARCH=$ARCH xbps-install -S -r /mnt -R "$REPO" "${BASE_SYSTEM[@]}"
 
 # TODO: configure time, language, etcs
 xgenfstab -U /mnt > /mnt/etc/fstab
